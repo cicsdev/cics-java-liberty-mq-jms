13a19d366e19210e201b1c6feb690d3c2c7953a7
Phil Wakelin <phil_wakelin@uk.ibm.com>
2017-07-27 16:03:12 +0100

    Branch1 (#1)
    * readme update
    
    * license correction
    
    * readme update
    
    * readme update
    
    * readme update
    
    * readme update
    
    * readme update
    
    * readme update
    
    * readme update
    
    * readme update
    
    * code review
    
    * readme update
    
    * add readtsq
    
    * readTSQ
    
    * add readtsq
    
    * readme update
    
    * code review

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 cics-java-liberty-mq-jms
 ================
-Sample JMS Java EE web application demonstrating how to use a JMS Connection Factory and a message-driven bean (MDB) to get and put messages to queues managed using IBM MQ Series
+Sample JMS Java EE web application demonstrating how to use a JMS Connection Factory and a message-driven bean (MDB) to get and put messages to queues managed using IBM MQ
+
+
+## Repository structure
+
+* [`projects/`](projects) - Eclipse projects suitable for importing into a CICS Explorer environment.
 
 ## Samples overview
 
@@ -10,123 +15,118 @@ Sample JMS Java EE web application demonstrating how to use a JMS Connection Fac
 * `com.ibm.cicsdev.mqjms.cicsbundle` - CICS bundle project that references the EAR bundle part for deployment in a CICS bundle
 
 
-## Repository structure
-
-* [`etc/`](etc) - Supporting materials used to define CICS and z/OS resources needed for the samples.
-* [`projects/`](projects) - Complete Eclipse projects suitable for importing into a CICS Explorer environment.
 
 
 
 ## Pre-requisites
 
-* CICS TS V5.3 or later, due to the JMS2 support
-* Java SE 7 or later on the z/OS system.
-* Java SE 7 or later on the workstation.
-* Eclipse with CICS Explorer SDK V5.4 or later installed due to the Java EE 7 support
-* IBM MQ Series V8.0 or later on z/OS
-* IBM MQ Series V9.0 resource adapter
- 
+* CICS TS V5.3 with APAR PI67640 and APAR PI58375, or CICS TS V5.4
+* Java SE 7 or later on the z/OS system
+* Java SE 7 or later on the workstation
+* CICS Explorer V5.4 with the IBM CICS SDK for Java EE and Liberty feature installed [available here](https://developer.ibm.com/mainframe/products/downloads)
+* IBM MQ V8.0 or later on z/OS
+* IBM MQ Version 9.0.1 resource adapter, available from [Fix Central](http://www-01.ibm.com/support/docview.wss?uid=swg21633761) 
 
 ## Configuration
 
-The sample code can be deployed as an EAR file into a CICS Liberty JVM server. The MQJMSDemo servlet can then be used to get and put messages to the defined queues and using an MQ client mode connection to a remote queue manager. There is also an MDB provided which will received a message written to a MQ queue and then write this message to a CICS TSQ queue in the same unit of work. 
+The sample code can be deployed as an EAR file into a CICS Liberty JVM server. The MQJMSDemo servlet can then be used to get and put messages to the defined queues and using an MQ client mode connection to a remote queue manager. There is also an MDB provided which will receive a message written to a MQ queue and then write this message to a CICS TSQ queue in the same unit of work
 
 
 
 
 ### To import the samples into Eclipse
 
-1. Import the projects into CICS Explorer using **File -> Import -> General -> Existing** projects into workspace. 
-2. Resolve the build path errors on the Web and EAR projects using the following menu from each project: 
-3. **Build Path -> Configure Build Path -> Libraries -> Add Library -> CICS with Java EE and Liberty**. 
-4. Select the version of CICS TS for deployment (either CICS TS V5.3 or CICS TS V5.4)
+1. Import the projects into CICS Explorer using **File -> Import -> General -> Existing** projects into workspace
+1. Resolve the build path errors on the Web and EAR projects using the following menu from each project: **Build Path -> Configure Build Path -> Libraries -> Add Library -> CICS with Java EE and Liberty** and select the version of CICS TS for deployment (either CICS TS V5.3 or CICS TS V5.4)
 
-### To configure MQ
-Setup following resources in the MQ series queue manager
+### To configure IBM MQ
+Setup the following resources in the IBM MQ queue manager
 
  1. MQ channel - WAS.JMS.SVRCONN
  1. MQ queue - DEMO.SIMPLEQ 
  1. MQ queue - DEMO.MDBQUEUE - This must be defined as shareable
 
 ### To configure CICS
-1. Create a Liberty JVM server as described in [4 easy steps](https://developer.ibm.com/cics/2015/06/04/starting-a-cics-liberty-jvm-server-in-4-easy-steps/)
+1.Create a Liberty JVM server as described in [4 easy steps](https://developer.ibm.com/cics/2015/06/04/starting-a-cics-liberty-jvm-server-in-4-easy-steps/)
 
-1. Download and install the MQ V9 RAR. This is available at [Fix Central](http://www-01.ibm.com/support/docview.wss?uid=swg21633761) and then define the RAR in the Liberty server.xml configuration file as follows:
+2.Download and install the MQ V9 RAR and then define the RAR in the Liberty server.xml configuration file as follows:
 `<variable name="wmqJmsClient.rar.location" value="/u/cics1/RARs/wmq.jmsra.rar"/>`
 
-1. Ensure the following Liberty features are present in server.xml
+3.Ensure the following Liberty features are present in server.xml
  ```xml
 <feature>cicsts:core-1.0</feature>
-<feature>jsp-2.3</feature>
-<feature>jms-2.0</feature>
 <feature>wmqJmsClient-2.0</feature>
-<feature>jmsMdb-3.2</feature>
+<feature>mdb-3.2</feature>
+<feature>jndi-1.0</feature>
 ```
-1. Add a JMS connection factory definition to the server.xml as follows:
+4.Add a JMS connection factory definition to the server.xml as follows:
 ```xml
 <jmsQueueConnectionFactory connectionManagerRef="ConMgr" jndiname="jms/qcf1">
-        <properties.wmqJms channel="WAS.JMS.SVRCONN" 
-                           hostName="localhost" 
-                           port="1420" queueManager="QM2C" 
-                           transportType="CLIENT"/>
-    </jmsQueueConnectionFactory>
-    <connectionManager id="ConMgr" maxPoolSize="10"/>  
+    <properties.wmqJms channel="WAS.JMS.SVRCONN" 
+        hostName="localhost" 
+        port="<your port>" queueManager="<your queue manager>" 
+        transportType="CLIENT"/>
+</jmsQueueConnectionFactory>
+    
+<connectionManager id="ConMgr" maxPoolSize="10"/>  
 ```
-1. Add a definition for the queues required by the test as follows:
+5.Add a definition for the queues required by the test as follows:
 ```xml
 <jmsQueue id="jms/simpleq" jndiName="jms/simpleq">
-		<properties.wmqJms baseQueueName="DEMO.SIMPLEQ" />
+    <properties.wmqJms baseQueueName="DEMO.SIMPLEQ" />
 </jmsQueue>
 <jmsQueue id="jms/mdbq" jndiName="jms/mdbq">
-		<properties.wmqJms baseQueueName="DEMO.MDBQUEUE" />
+    <properties.wmqJms baseQueueName="DEMO.MDBQUEUE" />
 </jmsQueue>  
 ```
-1. Add a JMS activation spec to the server.xml to define our MDB that will be invoked from the MDB queue. 
+6.Add a JMS activation spec to the server.xml to define our MDB that will be invoked from the MDB queue. 
         
 ```xml
-	<jmsActivationSpec id="mySimpleJMSEAR/mySimpleJMSMDB/MySimpleMDB">
-		<properties.wmqJms channel="WAS.JMS.SVRCONN"
-			destinationRef="jms/mdbq" destinationType="javax.jms.Queue"
-			port="1420" queueManager="QM2C"
-			transportType="CLIENT" />
-	</jmsActivationSpec>
+<jmsActivationSpec id="mySimpleJMSEAR/mySimpleJMSMDB/MySimpleMDB">
+    <properties.wmqJms channel="WAS.JMS.SVRCONN"
+        destinationRef="jms/mdbq" destinationType="javax.jms.Queue"
+        port="<your port>" queueManager="<your queue manager>"
+        transportType="CLIENT" />
+</jmsActivationSpec>
 ```
 
-The jmsActivationSpec ID must match the xxxx this, will be output in the Liberty messages.log in the following CNTR0180I message:  
+The jmsActivationSpec must be in the format of application name/module name/bean name, and is output in the Liberty messages.log in the following CNTR0180I message:  
 `[6/21/17 10:42:46:223 BST] 00000076 com.ibm.ws.ejbcontainer.runtime.AbstractEJBRuntime           I CNTR0180I: The MySimpleMDB message-driven bean in the mySimpleJMSMDB.jar module of the mySimpleJMSEAR application is bound to the com.ibm.cicsdev.mqjms.ear/com.ibm.cicsdev.mqjms.mdb/MySimpleMDB activation specification`    
 
 The `hostName`, `port`, `queueManager` properties in each of these elements should be set based on your local MQ configuration. The `transportType` property must be set to `CLIENT`
 
-1. Define and install a CICS TSQ named `RJMSTSQ` defined as recoverable, if you want to run the MDB test
+7.Define and install a CICS TSMODEL resource named RJMSTSQ with the attribute RECOVERY(YES) if you want to run the MDB test
 
  
 
-### To deploy the samples into a CICS region 
+### To deploy the sample into a CICS region 
 1. Change the name of the JVMSERVER in the .warbundle file from DFHWLP to the name of the JVMSERVER resource defined in CICS. 
 1. Using the CICS Explorer export the com.ibm.cicsdev.mqjms.cicsbundle project to a zFS directory. 
-1. Define and install a CICS `BUNDLE` resource definition referring to the deployed bundle directory on zFS in step 2, and ensure all resources are enabled. 
+1. Define and install a CICS BUNDLE resource definition referring to the deployed bundle directory on zFS in step 2, and ensure all resources are enabled. 
 
-## Running the tests
+## Running the sample
 
 
 * The Web application is configured with a context root of *jmsweb* so to invoke the servlet to write records to the simple JMS queue specify the test=putQ parameter after the context root for example:
-[http://host:port/jmsweb?test=putQ](http://host:port/jmsweb?test=putQ)  
+[http://host:port/jmsweb?test=putQ](http://host:port/jmsweb?test=putq)  
 
 If the test is successful, you will see the following response written to the browser:  
 `22/06/2017 16:11:20 3 records have been written to queue:///DEMO.SIMPLEQ`
 
 * To read the records back specify the readQ parameter:
-[http://host:port/jmsweb?test=readQ](http://host:port/jmsweb?test=readQ)
+[http://host:port/jmsweb?test=readQ](http://host:port/jmsweb?test=readq)
 
 * To write records to the MDB queue specify the putmdbQ parameter:
-[http://host:port/jmsweb?test=putmdbQ](http://host:port/jmsweb?test=putmdbQ)  
+[http://host:port/jmsweb?test=putmdbQ](http://host:port/jmsweb?test=putmdbq)  
 
-To verify that the MDB has been triggered, you can browse the contents of the TSQ RJMSTSQ as the MDB will write the messages from the MDBQUEUE to this CICS TSQ. 
+* To verify that the MDB has been triggered, you can read the contents of the CICS TSQ using the readTSQ test parameter:
+[http://host:port/jmsweb?test=readtsq](http://host:port/jmsweb?test=readtsq)
+
 
 ## Reference
 *  IBM Knowledge Center [Deploying message-driven beans to connect to IBM MQ](https://www.ibm.com/support/knowledgecenter/en/was_beta_liberty/com.ibm.websphere.wlp.nd.multiplatform.doc/ae/twlp_dep_msg_mdbwmq.html)
-*  MQRC_OBJECT_IN_USE when an MDB tries to get a message [Defining MDB queues as shareable](http://www-01.ibm.com/support/docview.wss?uid=swg21232930)
-* For further details on the JCICS APIs used in this sample refer to this [developer center article](https://developer.ibm.com/cics/2017/02/27/jcics-the-java-api-for-cics/)
+*  MQRC_OBJECT_IN_USE if the MDB tries to get a message from a queue that is not defined as shareable [Defining MDB queues as shareable](http://www-01.ibm.com/support/docview.wss?uid=swg21232930)
+*  For further details on the JCICS APIs used in this sample refer to this [developer center article](https://developer.ibm.com/cics/2017/02/27/jcics-the-java-api-for-cics/)
 
 
 ## License
--- projects/com.ibm.cicsdev.mqjms.web/src/com/ibm/cicsdev/mqjms/web/MQJMSDemo.java
+++ projects/com.ibm.cicsdev.mqjms.web/src/com/ibm/cicsdev/mqjms/web/MQJMSDemo.java
@@ -2,7 +2,7 @@
 /*                                                                        */
 /* SAMPLE                                                                 */
 /*                                                                        */
-/* (c) Copyright IBM Corp. 2016 All Rights Reserved                       */
+/* (c) Copyright IBM Corp. 2017 All Rights Reserved                       */
 /*                                                                        */
 /* US Government Users Restricted Rights - Use, duplication or disclosure */
 /* restricted by GSA ADP Schedule Contract with IBM Corp                  */
@@ -16,9 +16,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.jms.JMSContext;
-import javax.jms.JMSException;
 import javax.jms.JMSProducer;
+import javax.jms.JMSRuntimeException;
 import javax.jms.JMSConsumer;
+
 import javax.jms.ConnectionFactory;
 import javax.jms.Queue;
 import javax.jms.TextMessage;
@@ -31,253 +32,347 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ibm.cics.server.ItemHolder;
+import com.ibm.cics.server.TSQ;
+
 /**
- * Servlet implementation of JMSMQ Demo 
+ * Servlet implementation of JMS MQ demo
  * 
  */
 
 @WebServlet("/MQJMSDemo")
-public class MQJMSDemo extends HttpServlet
-{
+public class MQJMSDemo extends HttpServlet {
 
-	/**
-	 * tiemout for read of queue 
-	 */
-	private static final long TIMEOUT = 1000;
+	/** CICS local ccsid */
+	private static final String CCSID = System.getProperty("com.ibm.cics.jvmserver.local.ccsid");
 
-	/**
-	 * Name of the JMS connection factory
-	 */
+	/** timeout for read of queue in ms */
+	private static final long TIMEOUT = 100;
+
+	/** loop count for read of queue */
+	private static final int READ_COUNT = 10;
+
+	/** name of the JMS connection factory */
 	private static final String JMS_CF1 = "jms/qcf1";
 
-	/**
-	 * name of the standard JMS queue for puts/gets
-	 */
+	/** name of the standard JMS queue for puts & gets */
 	private static final String JMS_SIMPLEQ = "jms/simpleq";
 
-	/**
-	 * name of the JMS MDB queue
-	 */
+	/** name of the JMS MDB queue */
 	private static final String JMS_MDBQ = "jms/mdbq";
 
-	/**
-	 * Connection factory object
-	 */
-	private ConnectionFactory qcf = null;
+	/** JMS connection factory */
+	private static ConnectionFactory qcf;
+
+	/** JMS queue for CF tests */
+	private static Queue simpleq;
+
+	/** JMS Queue object for MDB test */
+	private static Queue mdbq;
+
+	/** Time format */
+	private static SimpleDateFormat dfTime = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+	/** CICS TSQ that will be written to */
+	private static final String TSQNAME = "RJMSTSQ";
+
+	/** Maximum TSQ read depth */
+	private static final int DEPTH_COUNT = 100;
 
 	/**
-	 * JMS Queue object for the JMS tests
-	 */
-	private Queue simpleq = null;
-
-	/**
-	 * JMS Queue object for the MDB test
-	 */
-
-	private Queue mdbq = null;	
-
-	/**
-	 * Servlet initialisation method called only on start of web app
-	 * 
-	 * Used to query and cache JNDI references   
+	 * Servlet initialisation method called only on initialisation of web app
 	 *
-	 */	
+	 * @param config
+	 *            - servlet configuration
+	 * @throws ServletException
+	 *             - if an error occurs
+	 */
 	public void init(ServletConfig config) throws ServletException {
 
-		/* JNDI lookups for all the JNDI strings  in this test */		
+		// JNDI lookups for all the JNDI strings in this test
 		try {
 			InitialContext ctx = new InitialContext();
 			qcf = (ConnectionFactory) ctx.lookup(JMS_CF1);
-			simpleq = (Queue) ctx.lookup(JMS_SIMPLEQ);			
+			simpleq = (Queue) ctx.lookup(JMS_SIMPLEQ);
 			mdbq = (Queue) ctx.lookup(JMS_MDBQ);
 
-		} catch (NamingException e) {
-			e.printStackTrace();
+		} catch (NamingException ne) {
+			System.out.println(
+					formatTime() + " ERROR: " + ne.getMessage() + " on JNDI lookup in servlet initialisation ");
+			ne.printStackTrace();
+		}
+	}
+
+	/**
+	 * HTTP GET - to analyse HTTP request and invoke relevant test method
+	 *
+	 * @param request
+	 *            - HTTP request
+	 * @param response
+	 *            - HTTP response
+	 * @throws IOException
+	 *             - if an error occurs
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		// Initialise variables;
+		PrintWriter pw = response.getWriter();
+
+		// Get test param from HTTP request to determine with test to run
+		String test = request.getParameter("test");
+		if (test != null) {
+
+			try {
+				if (test.equalsIgnoreCase("readq")) {
+					readQ(request, response);
+				} else if (test.equalsIgnoreCase("putq")) {
+					putQ(request, response);
+				} else if (test.equalsIgnoreCase("putmdbq")) {
+					putmdbQ(request, response);
+				} else if (test.equalsIgnoreCase("readtsq")) {
+					readTSQ(request, response);
+				} else if (test.isEmpty()) {
+					printWeb(pw, "Empty test param specified");
+				} else {
+					printWeb(pw, "Invalid test param specified: " + test);
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			printWeb(pw, "No test param specified: ");
+
 		}
 
 	}
 
 	/**
-	 * HTTP GET - to analyse HTTP request and invoke relevant test method 
-	 *
-	 */	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		/*
-		 *  print writer for output stream		 
-		 */
-		PrintWriter pw = response.getWriter();
-
-		// Reflect on the input test parm from the query string and call the relevant test method
-		String test = request.getParameter("test");
-		try {
-
-			getClass().getMethod(test, HttpServletRequest.class, HttpServletResponse.class).invoke(this, request,
-					response);			
-
-		} catch (NoSuchMethodException e) {
-			printWeb(pw, "No such test: " + test);
-
-		} catch (NullPointerException npe) {
-			if (test == null ) {
-				printWeb(pw, "ERROR: No test parameter supplied");
-			}
-			else {
-				printWeb(pw, "NPE in requested test: " + test);
-				printWeb(pw, npe.toString());
-			}
-		} catch (Exception e){
-			printWeb(pw, "ERROR in requested test: " + test);
-			printWeb(pw, e.toString());
-		}			
-	}
-
-	/**
-	 * Example reading msgs from a JMS queue 
+	 * Read a JMS queue and construct a HTTP response
 	 *
 	 * @param request
-	 *            HTTP request
+	 *            - HTTP request
 	 * @param response
-	 *            HTTP response
-	 * @throws IOException
-	 *             if an error occurs.
-	 */	
-	public void readQ(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	 *            - HTTP response
+	 * @throws Exception
+	 *             - if an error occurs.
+	 */
+	public void readQ(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		/*
-		 *  print writer for output stream		 
-		 */
-		PrintWriter pw = response.getWriter();		
+		// Initialise variables;
+		PrintWriter pw = response.getWriter();
+		String webmsg;
+		JMSConsumer consumer;
+		JMSContext context = null;
 
+		// Connect to the QM by creating the JMS context
+		try {
+
+			context = qcf.createContext();
+
+		} catch (JMSRuntimeException jre) {
+			webmsg = " ERROR: " + jre.getMessage() + " on connection to QM ";
+			printWeb(pw, webmsg);
+			jre.printStackTrace();
+		}
 
 		// Read contents of queue and construct a response
 		try {
-			JMSContext context = qcf.createContext();
-			JMSConsumer consumer = context.createConsumer(simpleq);
 
-			String title = "MQ records read from " + simpleq.getQueueName() + " are as follows:";		
-			printWeb (pw, title);
+			if (context != null) {
+				consumer = context.createConsumer(simpleq);
 
-			TextMessage msg;
-			do {
-				msg = (TextMessage) consumer.receive(TIMEOUT);
-				if ( msg != null) {
-					printWeb (pw, msg.getText());
+				// Read first batch of messages from the queue
+				webmsg = "First " + READ_COUNT + " records read from " + simpleq.getQueueName() + " are as follows:";
+				printWeb(pw, webmsg);
+
+				TextMessage txtmsg;
+				for (int i = 0; i < READ_COUNT; i++) {
+					txtmsg = (TextMessage) consumer.receive(TIMEOUT);
+					if (txtmsg != null) {
+						printWeb(pw, txtmsg.getText());
+					}
 				}
-			} while (msg != null);
+			}
 
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}			
+		} catch (JMSRuntimeException jre) {
+			webmsg = "ERROR on JMS receive from " + simpleq.getQueueName() + " jre.getMessage()";
+			printWeb(pw, webmsg);
+			jre.printStackTrace();
+		}
 	}
 
 	/**
-	 * Example writing msgs to a JMS queue 
+	 * Write to a JMS queue and construct an HTTP response
 	 *
 	 * @param request
-	 *            HTTP request
+	 *            - HTTP request
 	 * @param response
-	 *            HTTP response
-	 * @throws IOException
-	 *             if an error occurs.
-	 */	
-	public void putQ(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	 *            - HTTP response
+	 * @throws Exception
+	 *             - if an error occurs.
+	 */
+	public void putQ(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		/*
-		 *  print writer for output stream		 
-		 */
+		// Initialise objects;
 		PrintWriter pw = response.getWriter();
-
-		/*
-		 *  initialise strings		 
-		 */
 		String cicsmsg = formatTime() + " Simple JMS message for CICS";
+		String msgStr;
 
-		/*
-		 *  Create JMS context from CF and then put 3 msgs on the queue	 
-		 */
+		JMSContext context = null;
+		JMSProducer producer;
+
+		// Connect to the QM by creating the JMS context
 		try {
-			JMSContext context = qcf.createContext();
-			JMSProducer producer = context.createProducer();
-			
-			// Create message to be written to the producer then send it 
-			TextMessage message = context.createTextMessage(cicsmsg);
-			producer.send(simpleq, message);
-			
-			// Log message back to browser
-			String title = "Message has been written to " + simpleq.getQueueName() ;			
-			printWeb(pw, title);
 
-		} catch (JMSException je) {
-			je.printStackTrace();
-		}		
+			context = qcf.createContext();
 
-	}	
+		} catch (JMSRuntimeException jre) {
+			msgStr = " ERROR: " + jre.getMessage() + " on connection to QM ";
+			printWeb(pw, msgStr);
+			jre.printStackTrace();
+
+		}
+
+		try {
+
+			// check we have a connection to MQ
+			if (context != null) {
+
+				// Create message to be written to the producer then send it
+				producer = context.createProducer();
+				producer.send(simpleq, cicsmsg);
+
+				// Log message back to browser
+				String title = "Message has been written to " + simpleq.getQueueName();
+				printWeb(pw, title);
+			}
+
+		} catch (JMSRuntimeException jre) {
+
+			msgStr = "ERROR on JMS send to " + simpleq.getQueueName() + " jre.getMessage()";
+			printWeb(pw, msgStr);
+			jre.printStackTrace();
+
+		}
+
+	}
+
 	/**
-	 * Example writing msgs to a queue with an associated MDB
+	 * Write to a JMS queue used for MDB testing
 	 *
 	 * @param request
-	 *            HTTP request
+	 *            - HTTP request
 	 * @param response
-	 *            HTTP response
+	 *            - HTTP response
 	 * @throws IOException
-	 *             if an error occurs.
-	 */	
+	 */
 	public void putmdbQ(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-
-		/*
-		 *  simple print writer for output stream		 
-		 */
+		// Initialise variables
 		PrintWriter pw = response.getWriter();
-
-		/*
-		 * Initialise strings
-		 */		
-		String webmsg = null;
+		String webmsg;
 		String cicsmsg = formatTime() + " Simple MDB message for CICS";
+		JMSContext context = null;
+		JMSProducer producer;
 
-		// Create context and producer from connection factory
-		JMSContext context = qcf.createContext();
-		JMSProducer producer = context.createProducer();
+		// Connect to the QM by creating the JMS context
+		try {
 
-		// Create message to be written to the producer then send it 
-		TextMessage message = context.createTextMessage(cicsmsg);
-		producer.setProperty("TSQNAME", "MDBQ");
-		producer.send(mdbq, message);
+			context = qcf.createContext();
+
+		} catch (JMSRuntimeException jre) {
+			System.out.println(formatTime() + " ERROR: " + jre.getMessage() + " on connection to QM ");
+			jre.printStackTrace();
+		}
 
 		try {
-			webmsg = "Record has been written to MDB " + mdbq.getQueueName() + "\n";
-		} catch (JMSException e) {
 
-			e.printStackTrace();
+			// check we have a connection to MQ
+			if (context != null) {
+
+				// Create producer and set the property to define the CICS TSQ
+				producer = context.createProducer();
+				producer.setProperty("TSQNAME", TSQNAME);
+
+				// Send the message to the MDB queue
+				producer.send(mdbq, cicsmsg);
+			}
+
+		} catch (JMSRuntimeException jre) {
+
+			webmsg = "ERROR: " + jre.getMessage() + " on put to MDB " + "\n";
+			printWeb(pw, webmsg);
+			jre.printStackTrace();
+			throw (jre);
+
 		}
-		printWeb(pw, webmsg);		
+
+		webmsg = "Record has been written to MDB queue " + "\n";
+		printWeb(pw, webmsg);
 
 	}
 
 	/**
+	 * Read the TSQ written to by the MDB and construct a HTTP response
+	 *
+	 * @param request
+	 *            - HTTP request
+	 * @param response
+	 *            - HTTP response
+	 * @throws Exception
+	 *             - if an error occurs.
+	 */
+	public void readTSQ(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		// Initialise variables;
+		PrintWriter pw = response.getWriter();
+		String webmsg;
+
+		// Construct the TSQ object and set the name
+		TSQ tsqQ = new TSQ();
+		tsqQ.setName(TSQNAME);
+
+		// holder object to receive the data from CICS
+		ItemHolder holder = new ItemHolder();
+
+		webmsg = "Records read from TSQ (" + TSQNAME + ") are as follows:";
+		printWeb(pw, webmsg);
+
+		for (int i = 1; i <= DEPTH_COUNT; i++) {
+
+			tsqQ.readItem(i, holder);
+			byte[] data = holder.getValue();
+			String strData = new String(data, CCSID);
+
+			webmsg = "Record[" + i + "] " + strData;
+			printWeb(pw, webmsg);
+
+		}
+
+	}
+
+	/**
+	 * Write a formatted message with a time stamp insert to the web
+	 *
 	 * @param pw
-              Print writer
-	 * @param msg 
-	 *        Input message string
-	 *        
-	 *        Send a formatted message with a time stamp insert to the print writer
+	 *            - Print writer
+	 * @param msg
+	 *            - Input message string
 	 */
 	public void printWeb(PrintWriter pw, String msg) {
 
-		pw.print(formatTime() + " " + msg + "\n");		
+		pw.print(formatTime() + " " + msg + "\n");
 
 	}
 
 	/**
-	 * @return 
-	 *     String formatted time stamp
+	 * Write a formatted message with a time stamp insert to the web
+	 *
+	 * @return String - formatted time stamp
 	 */
 	public String formatTime() {
-		SimpleDateFormat dfTime = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); 
+
 		String time = dfTime.format(new Date());
 		return time;
 	}
